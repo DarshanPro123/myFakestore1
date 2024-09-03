bdc1905781e9089573bafbf9fb23bd535378bc5d
DarshanPro123 <darshanpro187@gmail.com>
2024-09-03 23:12:59 +0530

    delete home and cart make to home for some time usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import Home from "./components/Home/Home.jsx";
 import Carts from "./components/Carts/Carts.jsx";
 import AddTocart from "./components/AddTocart/AddTocart.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -12,7 +11,7 @@ function App() {
         <Router>
           <Header />
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Carts />} />
             <Route path="/carts" element={<Carts />} />
             <Route path="/addTocart" element={<AddTocart />} />
           </Routes>
